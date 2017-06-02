6aaae7f11ececf2d8fcf3fe07ae02dccee1af77a
theClubhou.se <heythere@theclubhou.se>
2017-06-02 16:08:19 -0400

    commit

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -57,7 +57,7 @@ export default class Home extends React.PureComponent {
       color:"#ffffff",
       backgroundSize:"100% 100%",
       marginLeft:"50px",
-      fontSize:"30px"
+      fontSize:"30px",
 
     }
     const navLink={
@@ -103,11 +103,13 @@ export default class Home extends React.PureComponent {
     }
     const paragraphStyle={
      display:"flex",
-     fontSize:"15px",
-     justifyContent:"center",
+     fontSize:"25px",
+     justifyContent:"row",
      color:"#ffffff",
      fontFamily:"Roboto, sans serif",
-     textAlign:"center"
+     textAlign:"center",
+     width:"300px",
+     height:"100px"
 
 
     }
@@ -116,15 +118,12 @@ export default class Home extends React.PureComponent {
       flexDirection:"row",
       justifyContent:"space-around",
       alignItems:"space-around",
-      marginTop:"50px"
-
-
-
+      marginTop:"100px",
     }
     const boxTwo={
       width:"300px",
       height:"300px",
-      background:"url(http://g01.a.alicdn.com/kf/HTB1.Y6AHVXXXXXKXVXXq6xXFXXXI/1-22-Joy-DIY-Blank-Kraft-paper-cardboard-boxes-for-gifts-packaging-sliding-gift-drawer-boxes.jpg_220x220.jpg)",
+      background:"url(http://www.grandforksherald.com/sites/default/files/styles/square_300/public/field/image/Camping-Tent_2.jpg?itok=e0KYNrmP)",
       backgroundSize:"100% 100%"
 
 
@@ -132,7 +131,7 @@ export default class Home extends React.PureComponent {
     const boxThree={
       width:"300px",
       height:"300px",
-      background:"url()",
+      background:"url(http://www.bestsolarlighting.com/wp-content/uploads/2017/05/ThorFire-Solar-LED-USB-Rechargeable-Camping-Lantern-300x300.jpg)",
 
 
 
@@ -140,14 +139,79 @@ export default class Home extends React.PureComponent {
     const boxFour={
       width:"300px",
       height:"300px",
-      background:"url()",
+      background:"url(https://images-na.ssl-images-amazon.com/images/S/aplus-seller-content-images-us-east-1/ATVPDKIKX0DER/A1YFTV540Q9MWH/B00PT10RSA/V1fh6sVQcmS._UX300_TTW_.jpg)",
 
 
 
     }
     const h1={
       fontFamily:"Roboto, sans serif",
-      fontSize:"50px"
+      fontSize:"80px",
+      alignSelf:"center"
+    }
+
+    const article={
+      fontSize:"20px",
+      fontFamily:"Railway"
+    }
+    const h2={
+      display:"flex",
+      flexDirection:"row",
+      width:"100%",
+      height:"300px",
+      justifyContent:"space-around",
+      fontFamily:"Roboto, sans serif",
+      fontSize:"20px",
+    }
+    const container={
+      display:"flex",
+      flexDirection:"row",
+      width:"100%",
+      height:"500px",
+      justifyContent:"space-around",
+      justifyContent:"center"
+
+    }
+    const imageStyle2={
+      background:"url(http://h4z.it/Image/d8b85c_4598_960_720.png)",
+      backgroundSize:"cover",
+      width:"600px",
+      height:"400px"
+
+    }
+    const article2={
+      fontSize:"20px",
+      fontFamily:"Railway"
+
+    }
+    const menu={
+      display:"flex",
+      flexDirection:"column",
+      width:"100%",
+      height:"300px",
+      justifyContent:"space-around",
+      fontFamily:"Roboto, sans serif",
+      fontSize:"20px",
+
+    }
+    const menuStyle={
+      fontSize:"10px",
+      fontFamily:"Railway"
+
+    }
+    const footer={
+      display:"flex",
+      flexDirection:"column",
+      width:"100%",
+      height:"200px",
+
+    }
+    const link={
+      background:"url(http://blogs.burton.com/blogs/media/images/17BG-AnnieJ-06.2e16d0ba.fill-1000x800-c75.jpg)",
+      backgroundSize:"cover",
+      width:"100%",
+      height:"800px"
+
     }
     return (
       <div>
@@ -158,7 +222,7 @@ export default class Home extends React.PureComponent {
           <div>
           <header style={headerStyle}>
             <nav style={navBar}>
-              <div style={logoStyle}>SWASH</div>
+              <div style={logoStyle}></div>
               <Link to="/Home" style={navLink}>Home</Link>
               <Link to="/Products" style={navLink}>Products</Link>
               <Link to="/About" style={navLink}>About</Link>
@@ -179,6 +243,42 @@ export default class Home extends React.PureComponent {
             <div style={boxThree}></div>
             <div style={boxFour}></div>
           </div>
+
+          <div style={h2}>
+            <div style={article}>SWASH TENTS</div>
+            <div style={article}>LED LATTERNS</div>
+            <div style={article}>HAMMICKS</div>
+          </div>
+
+          <div style={container}>
+            <div style={article2}></div>
+            <div style={imageStyle2}></div>
+            <div style={article2}></div>
+
+          </div>
+          <div style={menuStyle}>
+          <ul style={menu} class="side-nav header-links">
+	           <li><a data-wsid="ws-signin" class="signInLink" href="/my_account/index.jsp">Sign In</a></li>	<li><a data-wsid="ws-register" href="/my_account/index.jsp">Register</a></li>
+	           <li><a href="/catalog_request/index.jsp">Request A FREE Catalog</a></li>
+					<div class="show-for-small">
+						 <li><a data-reveal-id="emailSignUp" href="#" data-wsid="ws-emailsignup">Sign Up For Email Offers</a></li>
+					</div>
+						<li><a href="/shopCatalogs">Shop Our Catalogs</a></li>
+
+						<li data-wsid="ws-quickorderEF"><a data-wsid="ws-quickorder" href="/catalog/catalog_quick_order.jsp">Quick Order Form</a></li>
+						<li data-wsid="ws-manageaccountEF"><a data-wsid="ws-manageaccount" href="/my_account/manage_account.jsp">Your Account</a></li>
+	          <li><a href="/my_account/order_history/order_history_listing.jsp">Order Status</a></li>
+	          <li><a href="/content/faq">Frequently Asked Questions</a></li>
+	          </ul>
+
+            <footer style={footer}>
+              <div style={link}></div>
+            </footer>
+            </div>
+
+
+
+
 
 
 
